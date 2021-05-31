580988c5373010f386f884b6ff5af6f3eba7a192
Ayush <Ayush.verma@vtnetzwelt.com>
2021-05-31 16:30:33 +0530

    Production ready project

--- graphql/context/index.js
+++ graphql/context/index.js
@@ -9,16 +9,9 @@ const verifyToken = async ( token ) =>
 {
     try
     {
-        //if header does not contain a router
         if ( !token ) return null;
-
-        //verify jwt
         const { email } = await jwt.verify( token, process.env.TOKEN_SECRET );
-
-        //find user 
         const user = await User.findOne( { where: { email } } );
-
-        //if user is fetched successfully then return user
         return user;
 
     } catch ( error )
